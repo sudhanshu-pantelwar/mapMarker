78481d62ffb72c427b596c73c9ef389e91c82092
sudhanshu-pantelwar <sudhanshu.pantelwar@gmail.com>
2017-11-10 15:24:07 +0530

    Branch

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -56,7 +56,7 @@ export class HomePage {
         })
       }, (err) => {
         console.log(JSON.stringify(err));
-        console.log('Jawla Test');
+        console.log('Branch');
       });
     }
     
